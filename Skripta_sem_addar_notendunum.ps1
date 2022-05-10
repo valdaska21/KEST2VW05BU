a4ed1f0a730cd22e69218f4bdb8b21b4089933a8
Valdas Kaubrys <89401307+valdaska21@users.noreply.github.com>
2022-05-10 10:05:35 -0700

    fyrir lokaverkefni, Skriptan sem addar notendum

---
+++ Skripta_sem_addar_notendunum.ps1
@@ -0,0 +1,7 @@
+﻿$gogn = Import-Csv C:\Users\SetupUser\Desktop\notendur.csv
+foreach($lina in $gogn) {
+    New-LocalUser $lina.notendanafn -FullName $lina.nafn -NoPassword
+    Add-LocalGroupMember -Group $lina.hopur -Member $lina.notendanafn
+    Add-LocalGroupMember -Group $lina.allirhopur -Member $lina.notendanafn
+    Add-LocalGroupMember -Group $lina.userhopur -Member $lina.notendanafn
+}
